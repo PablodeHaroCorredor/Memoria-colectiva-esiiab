5547662c30104c42b06818cef5b6d02b3ba1f09e
Pablo de Haro Corredor <pablodeharocorredor@gmail.com>
2022-01-11 11:27:01 +0100

    package.json modificado con npm version

--- node-api/app.js
+++ node-api/app.js
@@ -11,7 +11,7 @@ const nodemailer = require('nodemailer');
 const details = require("./details.json");
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../dist')))
+app.use(express.static(path.join(__dirname, '../dist/memoria-colectiva-esiiab')))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
@@ -96,7 +96,7 @@ let verifySession = (req, res, next) => {
 
 app.get('*',function(req, res, next){
     res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/memoria-colectiva-esiiab/index.html'));
 });
 
 
