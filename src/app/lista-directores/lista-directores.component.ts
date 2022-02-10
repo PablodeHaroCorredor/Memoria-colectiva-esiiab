a479cea7a8ee44dea8ea05a6b58ffaf71663a41f
Pablo de Haro Corredor <pablodeharocorredor@gmail.com>
2022-02-10 18:09:17 +0100

    mejoras visuales

--- src/app/lista-directores/lista-directores.component.ts
+++ src/app/lista-directores/lista-directores.component.ts
@@ -13,6 +13,7 @@ export class ListaDirectoresComponent implements OnInit {
   flag=true;
   flag2=false;
   directores:any
+  nombresDirector:any
   constructor(private directorService: DirectorService, private route: ActivatedRoute) { 
   }
  ngOnInit(){
@@ -21,12 +22,26 @@ export class ListaDirectoresComponent implements OnInit {
       (params: Params) => {
           this.directorService.getDirectores().subscribe((directores: any) => {
             this.directores = directores;
-         
+            this.nombresDirector =this.getNombreDirector()
           }
           
           )} 
+         
     )
+    
   }
+
+  getNombreDirector(){
+    var lista:string[] =[]
+    for(let director of this.directores){
+      lista.push(director.nombre)
+    }
+
+    return lista
+  }
+
+
+
    
 
   
--- src/app/lista-optativas/lista-optativas.component.ts
+++ src/app/lista-optativas/lista-optativas.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaOptativasComponent implements OnInit {
 
+  optativas:string[] =["Visión Artificial y Reconocimiento de Patrones","Criptografia", "Robótica Autónoma", 
+  "Auditoría en Sistemas de Información", "Ingeniería Web y de Servicios", "Dispositivos y Redes Inalámbricos", "Aceleradores Gráficos",
+   "Diseño Gráfico y Animación", "Inteligencia Artificial en Videojuegos", "Videojuegos y Realidad Virtual"]
   constructor() { }
 
   ngOnInit(): void {
